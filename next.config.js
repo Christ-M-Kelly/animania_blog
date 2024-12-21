7218570e6c9372f9dbc13cbc1c57bbc568af3f95
Mehdibkb <mehdi-yacine.boukabou@supdevinci-edu.fr>
2024-12-21 11:58:38 +0100

    résolution du conflit dans next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const config = {
   images: {
     domains: ["localhost", "xw8oikocbczvr6wo.public.blob.vercel-storage.com"],
-=======
-const nextConfig = {
-  transpilePackages: ["@fortawesome/fontawesome-free"],
-  images: {
-    domains: ["xw8oikocbczvr6wo.public.blob.vercel-storage.com"],
->>>>>>> 108f84205b2fb17193a7c96d7ac6c52f3878318b
   },
 };
 
